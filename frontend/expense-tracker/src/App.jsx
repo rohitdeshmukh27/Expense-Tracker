f5faa71e108c4fbac7d4a7f66bf02b76ab58de92
Rohit <rohitvd27@gmail.com>
2025-06-06 14:48:41 +0530

    Defined Routes + favicon updated & title updated

--- frontend/expense-tracker/src/App.jsx
+++ frontend/expense-tracker/src/App.jsx
@@ -1,7 +1,32 @@
 import React from "react";
 
+import {
+  BrowserRouter as Router,
+  Routers,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Login from "./pages/Auth/Login";
+import SignUp from "./pages/Auth/SignUp";
+import Home from "./pages/Dashboard/Home";
+import Income from "./pages/Dashboard/Income";
+import Expense from "./pages/Dashboard/Expense";
+
 const App = () => {
-  return <div className="text-3xl text-purple-800">App</div>;
+  return (
+    <div>
+      <Router>
+        <Router>
+          <Route path="/" element={<Root />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signUp" element={<SignUp />} />
+          <Route path="/dashboard" element={<Home />} />
+          <Route path="/income" element={<Income />} />
+          <Route path="/expense" element={<Expense />} />
+        </Router>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
